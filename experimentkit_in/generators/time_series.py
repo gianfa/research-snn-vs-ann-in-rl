9f8da4997032a0311fb38a4bf6c06390abc80bf2
gianfa <gian.angelini@hotmail.com>
2023-05-31 00:02:38 +0200

    feat(expkit, generators): gen_lorenz

--- experimentkit_in/generators/time_series.py
+++ experimentkit_in/generators/time_series.py
@@ -123,3 +123,68 @@ def tsds_generate_periodic_sin_as_sum(
         baselines=baselines,
         axis=axis
     )
+
+
+# Dynamics
+
+def lorenz(xyz, *, s=10, r=28, b=2.667):
+    """
+    Parameters
+    ----------
+    xyz : array-like, shape (3,)
+       Point of interest in three-dimensional space.
+    s, r, b : float
+       Parameters defining the Lorenz attractor.
+
+    Returns
+    -------
+    xyz_dot : array, shape (3,)
+       Values of the Lorenz attractor's partial derivatives at *xyz*.
+
+    Credits
+    ---------
+    `<https://matplotlib.org/stable/gallery/mplot3d/lorenz_attractor.html>`_
+
+    """
+    x, y, z = xyz
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return np.array([x_dot, y_dot, z_dot])
+
+
+def gen_lorenz(
+        dt: float = 0.01,
+        n_steps: int = 10000,
+        xyz_0: tuple = (0., 1., 1.05),
+        s=10, r=28, b=2.667):
+    """Generate lorenz attractor time series
+
+    Parameters
+    ----------
+    dt : float, optional
+        dt, by default 0.01
+    n_steps : int, optional
+        Number of time steps, by default 10000
+    xyz_0 : tuple, optional
+        Initial Conditions, by default (0., 1., 1.05)
+    s : int, optional
+        s parameter, by default 10
+    r : int, optional
+        r parameter, by default 28
+    b : float, optional
+        b parameter, by default 2.667
+
+    Returns
+    -------
+    np.array
+        x-y-z time series, shaped as (num_steps +1) x 3
+    """
+    xyzs = np.empty((n_steps + 1, 3))  # Need one more for the initial values
+    xyzs[0] = xyz_0  # Set initial values
+    # Step through "time", calculating the
+    #   partial derivatives at the current point
+    #   and using them to estimate the next point
+    for i in range(n_steps):
+        xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i], s=s, r=r, b=b) * dt
+    return xyzs
