4cefcb05a193b83ec460a569b29650a35c8186f2
gianfa <gian.angelini@hotmail.com>
2023-11-02 21:45:33 +0100

    feat(experimentkit_in): logger and more

--- experimentkit_in/__init__.py
+++ experimentkit_in/__init__.py
@@ -1,1 +1,8 @@
+from . import collections
+# from . import environment
+from . import funx
+from . import logger_config
+from . import metricsreport
+from . import monitor
+from . import reporting
 from . import visualization
