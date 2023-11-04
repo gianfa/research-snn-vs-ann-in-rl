868542e45915b4444ffc642ab5b0789f57a8efcb
gianfa <gian.angelini@hotmail.com>
2023-11-04 14:45:37 +0100

    feat(exp14): 20units, d4, r4; + doc

--- experiments/14-LSM-rope-analysis/flows/_3_analyze.py
+++ experiments/14-LSM-rope-analysis/flows/_3_analyze.py
@@ -1,8 +1,8 @@
 """
 
 
-Dopo aver fatto una ricerca degli iperparametri più importanti,
-abbiamo scelto una configurazione da analizzare.
+After researching the most important hyperparameters,
+we chose a configuration to analyze.
 reservoir_size: 20, radius: 4, degree [2, 3].
 
 
@@ -40,52 +40,71 @@ from experimentkit_in.logger_config import setup_logger
 
 # %% Project Parameters
 
+prefixes = [
+    'S2-trial1',
+    'S2-trial2',
+    'S2-trial3',   # 
+    # 'S2-trial4', # d4, r4
+]
 
-RUN_PREFIX = f'S2-trial3'
-# data_path = EXP_DATA_DIR/"2freq_toy_ds-20000-sr_50-n_29.pkl"
+OUTPUT_DIR = EXP_REPORT_DIR/"topological_analysis"
 
-RUN_DIR = EXP_REPORT_DIR/RUN_PREFIX
-subrun_paths = [run_i for run_i in RUN_DIR.iterdir() if run_i.is_dir()]
+if not OUTPUT_DIR.exists():
+    os.mkdir(OUTPUT_DIR)
 
-exp_params = ek.funx.load_yaml(subrun_paths[0]/"params.pkl")
-degree = exp_params['LIF_LIF_connections']['degree']
-radius = exp_params['LIF_LIF_connections']['radius']
+for RUN_PREFIX in prefixes:
+    # data_path = EXP_DATA_DIR/"2freq_toy_ds-20000-sr_50-n_29.pkl"
 
-def collect_from_runs(x):
-    return [run_i/x for run_i in subrun_paths]
+    RUN_DIR = EXP_REPORT_DIR/RUN_PREFIX
+    subrun_paths = [run_i for run_i in RUN_DIR.iterdir() if run_i.is_dir()]
 
-topol_paths = collect_from_runs('reservoir_topology.pkl')
-topols = torch.stack([ek.funx.pickle_load(topol_path)['conn_lif_lif']
-          for topol_path in topol_paths])
+    exp_params = ek.funx.load_yaml(subrun_paths[0]/"params.yaml")
+    degree = exp_params['LIF_LIF_connections']['degree']
+    radius = exp_params['LIF_LIF_connections']['radius']
 
-acc_lastm_paths = collect_from_runs('results.pkl')
-acc_lastms = torch.stack([ek.funx.pickle_load(acc_lastm_path)['acc_last_mean']
-          for acc_lastm_path in acc_lastm_paths])
+    def collect_from_runs(x):
+        return [run_i/x for run_i in subrun_paths]
 
+    topol_paths = collect_from_runs('reservoir_topology.pkl')
+    topols = torch.stack([ek.funx.pickle_load(topol_path)['conn_lif_lif']
+            for topol_path in topol_paths])
 
-pos_perf = (acc_lastms.view(len(acc_lastms), 1, 1) * (topols!=0))
-""" Performance of the positions
-Each topology has the performance value in each of the cells"""
-
-fig, ax = plt.subplots()
-sns.heatmap(pos_perf.mean(0), vmin=0, vmax=1, ).set(
-    title=f'Mean performance per position\nr:{radius}, d:{degree}')
-fig.savefig(RUN_DIR/f"avg_acc_per_pos-ressize_20-d_{degree}-r_{radius}.png")
-
-pos_perf_filtered = torch.where(pos_perf.mean(0) > .25, pos_perf.mean(0), 0)
-
-fig, ax = plt.subplots()
-sns.heatmap(pos_perf_filtered, vmin=0, vmax=1, ax=ax).set(
-    title=f'Mean final acc per position > 0.25\nr:{radius}, d:{degree}')
-fig.savefig(RUN_DIR/f"avg_acc_per_pos-ressize_20-d_{degree}-r_{radius}.png")
+    acc_lastm_paths = collect_from_runs('results.pkl')
+    acc_lastms = torch.stack(
+        [ek.funx.pickle_load(acc_lastm_path)['acc_last_mean']
+            for acc_lastm_path in acc_lastm_paths])
 
 
-# %%
+    pos_perf = (acc_lastms.view(len(acc_lastms), 1, 1) * (topols!=0))
+    """ Performance of the positions
+    Each topology has the performance value in each of the cells"""
 
-print(topols.shape)
-sns.heatmap(topols.mean(0)).set(title='Mean weight values')
+    fig, ax = plt.subplots()
+    sns.heatmap(pos_perf.mean(0), vmin=0, vmax=1, cmap='coolwarm').set(
+        title=f'Mean performance per position\nr:{radius}, d:{degree}')
+    fname = OUTPUT_DIR/f"avg_acc_per_pos-ressize_20-d_{degree}-r_{radius}.png"
+    fig.savefig(fname)
+
+    pos_perf_filtered = torch.where(
+        pos_perf.mean(0) > .25, pos_perf.mean(0), 0)
+
+    fig, ax = plt.subplots()
+    sns.heatmap(pos_perf_filtered, vmin=0, vmax=1, cmap='coolwarm', ax=ax).set(
+        title=f'Mean final acc per position > 0.25\nr:{radius}, d:{degree}')
+    fname = OUTPUT_DIR/f"avg_acc_per_pos-ressize_20-d_{degree}-r_{radius}-25pc.png"
+    fig.savefig(fname)
+
+
+    # %%
+
+    print(topols.shape)
+    sns.heatmap(topols.mean(0)).set(title='Mean weight values', cmap='coolwarm')
 
 # %%
+
+# trial_prefix = 'S2'
+# for trial_i in EXP_REPORT_DIR.iterdir():
+#     if trial_i.name.startswith(trial_prefix):
 
 
 
--- experiments/14-LSM-rope-analysis/reports/topological-analysis.md
+++ experiments/14-LSM-rope-analysis/reports/topological-analysis.md
@@ -3,6 +3,41 @@
 ### The signal
 The signal on which the network was trained consists of a constant baseline and alternating fragments whose frequency can be one of two possible
 
+## The  network
+
+<img src='./imgs/network1.png'>
+
+The neural network is a Liquid State Machine (LSM) \[Maas, 2002\], with the following characteristics.
+
+### The reservoir
+
+* Total number of neurons: 20
+* Number of input neurons: 10
+* Number of output neurons: 10
+* Weights values randomly assigned (Normal)
+* Synaptic connections with decay
+   * Randomly distributed (Normal)
+   * decay function: $g[t+1] = g[t] * q$, with $g$ synaptic gain (conductance) and $q \in [0,1]$, decay rate.
+   * decay rate: .25
+
+### The Readout
+
+Fully connected layer.
+* Number of neurons equal to the number of categories to classify
+
+## The Training
+
+* Optimizer: Adam \[Kigma, 2014\]
+* Loss based on CrossEntropy
+
+At each training step the network receives a signal element.
+The network subsequently completes an entire propagation step between the layers until a prediction is reached.  
+A buffer of a specific size, sequentially collects a specific amount of network outputs.
+
+### The loss computation
+At a specific step interval, $l_{scope}$ (250), the loss is calculated over the the whole buffer.
+
+
 ## Experiment
 After researching the most important hyperparameters,
 we chose a configuration to analyze.  
@@ -22,13 +57,15 @@ Below we see a representation of the average performance obtained by connection 
 
 Each graph was obtained by multiplying the binary adjacency matrix of each run by $acc_{10p}$ and finally averaging for each cell.
 
-<img src='./S2-trial2/avg_acc_per_pos-ressize_20-d_2-r_4.png' width=50%>
-
-<img src='./S2-trial1/avg_acc_per_pos-ressize_20-d_3-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_1-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_2-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_3-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_4-r_4.png' width=50%>
 
 
 Here below we see reported the same values as in the graphs above, but filtered by $acc_{10p}$ > 0.25
 
-<img src='./S2-trial2/avg_acc_per_pos-ressize_20-d_2-r_4-25pc.png' width=50%>
-
-<img src='./S2-trial1/avg_acc_per_pos-ressize_20-d_3-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_1-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_2-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_3-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_4-r_4-25pc.png' width=50%>
