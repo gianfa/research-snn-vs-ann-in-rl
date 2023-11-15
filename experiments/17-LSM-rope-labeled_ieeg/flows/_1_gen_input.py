59bafc0e0190c5cce580be1cf8b02d76b9f47440
gianfa <gian.angelini@hotmail.com>
2023-11-15 08:14:59 +0000

    exp(17): ieeg, init

---
+++ experiments/17-LSM-rope-labeled_ieeg/flows/_1_gen_input.py
@@ -0,0 +1,154 @@
+""" Helper file to generate a input signal
+
+"""
+# %%
+
+from typing import Any
+
+import matplotlib.pyplot as plt
+import snntorch as snn
+import torch
+from torch import nn
+
+import os
+import pickle
+from pathlib import Path
+import sys
+
+import scipy.io
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path += ['../../']
+
+from _0_config import *
+
+CROSSBRAIN_DIR = (ROOT/'../CROSSBRAIN_signals').resolve()  # CROSSBRAIN_signals dir
+DISK_DIR = Path('/DATA1/crossbrain_data/')
+DATA_DIR = CROSSBRAIN_DIR/'data'
+
+# /DATA1/crossbrain_data/LFP_new_dataset/data_before_stimulation.p
+
+# %%
+
+assert CROSSBRAIN_DIR.resolve().name == "CROSSBRAIN_signals"
+assert DISK_DIR.exists(), f"DISK_DIR id not a valid path"
+
+
+# %%
+
+dataset_path: str or Path = DISK_DIR/"ieeg-sweez-eth_3patients/dataset.p"
+# dataset_path: str or Path = DISK_DIR/"ieeg-sweez-eth_3patients/dataset_BSA_encoded_bool.p"
+dataset_is_spike_encoded = False
+
+ds = pickle.load(open(dataset_path, "rb"))
+
+fragment_labels = torch.tensor([ds[i][0] for i in range(len(ds)) ])
+fragment_signals = torch.tensor(np.array([ds[i][1] for i in range(len(ds)) ]))  # voltage signal
+
+print(
+    f"fragment_labels.shape: {fragment_labels.shape}\fragment_signals.shape: {fragment_signals.shape}\n"
+)
+
+# %%
+"""
+Shuffle the dataset in order to have a random collection of labeled signals
+
+"""
+
+fig, axs = plt.subplots(1, 2)
+axs[0].plot(fragment_labels)
+axs[0].set_title("Labels, from loading")
+
+new_pos = torch.randperm(len(fragment_labels))
+axs[1].plot(fragment_labels[new_pos])
+axs[1].set_title("Labels, after permutation")
+fig.tight_layout
+
+# %%
+
+fragment_signals = fragment_signals[new_pos]
+fragment_labels = fragment_labels[new_pos]
+
+# %%
+
+if dataset_is_spike_encoded:
+    I = fragment_signals
+
+    # # Labels (y) definition
+    y = fragment_labels
+
+else:
+    num_signals = 30
+    signal_length = fragment_signals.shape[1] * (num_signals + 10) # signal length (simulation step)
+
+    dirty_signal = torch.zeros(signal_length)
+
+    min_spc = 25  # minimum intra-signals space
+    pp = 0  # pointer
+    sig_idxs = []
+    sig_labels = []
+    for i in range(num_signals):
+        if i > 0:
+            pp += min_spc
+        cur_sig = fragment_signals[i]
+        cur_idx_start = torch.randint(pp, pp + 100, size=(1, 1)).item()
+        cur_idx_end = cur_idx_start + len(cur_sig)
+        
+        if len(dirty_signal) < cur_idx_end:
+            print(f"Signal overflow at {i}")
+            break
+        dirty_signal[cur_idx_start:cur_idx_end] += cur_sig
+        pp = cur_idx_end
+        sig_idxs.append((cur_idx_start, cur_idx_end))
+        sig_labels[cur_idx_start:cur_idx_end] = torch.ones(cur_idx_end-cur_idx_start) * fragment_labels[i]
+
+    sig_idxs = torch.tensor(sig_idxs)
+    sig_labels = torch.tensor(sig_labels)
+
+
+    # visualize data set
+
+    fig, axs = plt.subplots(2, 1, sharex=True, figsize=(10, 10))
+
+    axs[0].plot(dirty_signal, label='dirty signal')
+    for i in range(num_signals):
+        idx_from = sig_idxs[i][0]
+        idx_to = sig_idxs[i][1]
+        axs[0].plot(
+            torch.arange(idx_from, idx_to),
+            dirty_signal[idx_from:idx_to], c='k')
+    axs[0].set_title("Input signal, $I$")
+    axs[0].grid()
+
+    axs[1].plot(
+        sig_labels, torch.ones_like(sig_labels),
+        marker='|', markersize=22, linestyle="", c='k')
+    axs[1].set_ylim(0.9)
+    axs[1].set_title('labels indices')
+    axs[1].get_yaxis().set_visible(False)
+    axs[1].set_xlim(0, 500)
+    fig.tight_layout()
+
+
+    I = dirty_signal
+
+    # # Labels (y) definition
+    y = sig_labels
+
+fig, axs = plt.subplots(2, 1, sharex=True)
+axs[0].plot(torch.arange(len(I)), I)
+axs[0].set_title('I')
+axs[0].set_ylabel('amplitude')
+axs[0].set_xlabel('t')
+axs[0].grid()
+
+axs[1].plot(torch.arange(len(y)), y, 'o')
+axs[1].set_title('Labels (y)')
+axs[1].set_ylabel('')
+axs[1].set_xlabel('t')
+axs[1].set_yticks([-1, 0, 1])
+axs[1].set_ylim(0, 2.3)
+axs[1].grid()
+fig.tight_layout()
+# %%
---
+++ experiments/17-LSM-rope-labeled_ieeg/reports/topological-analysis.md
@@ -0,0 +1,87 @@
+# Topological Analysis [TOREVIEW]
+
+### The Signal
+The signal on which the network was trained consists of a constant baseline and alternating fragments whose frequency can be one of two possible. The two possible frequencies are each one the result of the sum of a pair among 3 possibilities.
+
+## The Network
+
+<img src='./imgs/network2.png'>
+
+The neural network is a Liquid State Machine (LSM) \[Maas, 2002\], with the following characteristics.
+
+### The Reservoir
+
+* Total number of neurons: 20
+* Number of input neurons: 10
+* Number of output neurons: 10
+* Weights values randomly assigned (Normal)
+* Synaptic connections with decay
+   * Randomly distributed (Normal)
+   * decay function: $g[t+1] = q \cdot g[t]$, with $g$ synaptic gain (conductance) and $q \in [0, 1]$ decay rate.
+   * decay rate: .25
+
+### The Readout
+
+Fully connected layer.
+* Number of neurons equal to the number of categories to classify
+
+## The Training
+
+* Optimizer: Adam \[Kigma, 2014\]
+* Loss based on CrossEntropy
+
+At each training step the network receives a signal element.
+The network subsequently completes an entire propagation step between the layers until a prediction is reached.  
+A buffer of a specific size, sequentially collects a specific amount of network outputs.
+
+### The Loss Computation
+At a specific step interval, $l_{scope}$ (250), the loss is calculated over the the whole buffer.
+
+
+## Experiment
+After researching the most important hyperparameters,
+we chose a configuration to analyze.  
+* reservoir_size: 20,
+* radius: 4,
+* degree in \{2, 3\}.
+
+Comprehensive training was repeatedly launched and performance was collected.  
+
+100 runs were performed for each change in (degree, radius).
+
+The performance metrics chosen here is the average prediction accuracy in the last 10% portion of the signal, $acc_{10p}$.
+
+
+## Visualization
+Below we see a representation of the average performance obtained by connection position on the adjacency matrix.
+
+<img src='./topological_analysis/perf_x_pos-mean.png'>
+<!-- Each graph was obtained by multiplying the binary adjacency matrix of each run by $acc_{10p}$ and finally averaging for each cell.
+
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_1-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_2-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_3-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_4-r_4.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_3-r_3.png' width=50%> -->
+
+
+<!-- Here below we see reported the same values as in the graphs above, but filtered by $acc_{10p}$ > 0.25
+
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_1-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_2-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_3-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_4-r_4-25pc.png' width=50%>
+<img src='./topological_analysis/avg_acc_per_pos-ressize_20-d_3-r_3-25pc.png' width=50%> -->
+
+
+Here below we see the top neurons by mean performance
+
+<img src='./topological_analysis/top_perf_x_pos-mean.png'>
+
+Below we see a graph rapresentation of the top neuron positions
+
+<img src='./topological_analysis/top_perf_graph.png'>
+
+## Back to LSM Topological Analysis
+
+[LSM-topological_analysis](./../../../docs/LSM-topological_analysis.md)
