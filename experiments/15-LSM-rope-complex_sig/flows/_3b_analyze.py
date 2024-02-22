d186c21971e5ec9b7041f25afa2e3f711c45cd06
gianfa <gian.angelini@hotmail.com>
2024-02-22 23:57:09 +0100

    feat(experiments, 15): analyze xperiment

--- experiments/15-LSM-rope-complex_sig/flows/_3b_analyze.py
+++ experiments/15-LSM-rope-complex_sig/flows/_3b_analyze.py
@@ -9,6 +9,7 @@ reservoir_size: 20, radius: 4, degree [2, 3].
 """
 # %%
 from math import ceil
+from pathlib import Path
 import sys
 
 sys.path += ['../', '../../../']
@@ -24,6 +25,17 @@ from experimentkit_in.logger_config import setup_logger
 SAVE_DATA = False
 
 # %% -- Helpers --
+
+def plot_acc_hist(acc_hist, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
+    ax.plot(acc_hist)
+    ax.set_ylabel("Accuracy")
+    ax.set_xlabel("acc scope, after wash time")
+    return ax
+
 
 def plot_adjacency_map(
         adj: torch.tensor,
@@ -90,22 +102,39 @@ def connections_to_digraph(
     return G
 
 
+def moving_average(signal, window_size):
+    cumsum = np.cumsum(signal)
+    return (cumsum[window_size:] - cumsum[:-window_size]) / window_size
+
+
+def plot_signal_band_plot(signal, smooth=True, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
+
+    q1 = torch.quantile(signal, 0.25, dim=0)
+    q2 = torch.quantile(signal, 0.50, dim=0)
+    q3 = torch.quantile(signal, 0.75, dim=0)
+
+    if smooth:
+        w_size = 5
+        q1 = moving_average(q1, w_size)
+        q2 = moving_average(q2, w_size)
+        q3 = moving_average(q3, w_size)
+
+    ax.plot(range(len(q1)), q1, label="Pct25", color='black', linewidth=.7)
+    ax.plot(range(len(q2)), q2, label="Pct50", color='red', linewidth=.7)
+    ax.plot(range(len(q3)), q3, label="Pct75", color='black', linewidth=.7)
+    ax.fill_between(range(len(q1)), q1, q3, color='gray', alpha=0.3)
+    return ax
+
+
 # %% -- Project Parameters --
 
-trials = [
-    '15-trial-r4-d1',
-    '15-trial-r4-d2',
-    '15-trial-r4-d3',
-    '15-trial-r4-d4',
-    '15-trial-r3-d1',
-    '15-trial-r3-d2',
-    '15-trial-r3-d3',
-    '15-trial-r2-d2',
-    '15-trial-r2-d1',
-    '15-trial-r1-d1',
-]
 
-# trials = ['15-trial-NO-plastic']
+TRIALS_NAME_1 = '15-trial-NO-plastic'
+TRIALS_NAME_2 = '15-trial-YES-plastic'
 
 OUTPUT_DIR = EXP_REPORT_DIR/"topological_analysis"
 expected_run_n = 100
@@ -113,6 +142,124 @@ expected_run_n = 100
 if not OUTPUT_DIR.exists():
     os.mkdir(OUTPUT_DIR)
 
+
+# %%
+n_samples = 100
+
+trials_NO_dir = Path(EXP_DATA_DIR/f"experiments/{TRIALS_NAME_1}")
+assert trials_NO_dir.is_dir()
+
+subtrial_NO_dirs = list(trials_NO_dir.iterdir())[:n_samples]
+
+print(f"subtrial_NO_dirs: {len(subtrial_NO_dirs)}")
+
+trials_YES_dir = Path(EXP_DATA_DIR/f"experiments/{TRIALS_NAME_2}")
+assert trials_YES_dir.is_dir()
+
+subtrial_YES_dirs = list(trials_YES_dir.iterdir())[:n_samples]
+
+print(f"subtrial_YES_dirs: {len(subtrial_YES_dirs)}")
+
+# %% Load results
+
+trials_NO_results = [
+    ek.funx.pickle_load(s/"results.pkl") for s in subtrial_NO_dirs]
+trials_YES_results = [
+    ek.funx.pickle_load(s/"results.pkl") for s in subtrial_YES_dirs]
+
+# %%
+
+trials_NO_results_acc_last_mean = [
+    x['acc_last_mean'] for x in trials_NO_results]
+trials_NO_results_acc_hist_list = [x['acc_hist'] for x in trials_NO_results]
+
+
+trials_YES_results_acc_last_mean = [
+    x['acc_last_mean'] for x in trials_YES_results]
+trials_YES_results_acc_hist_list = [x['acc_hist'] for x in trials_YES_results]
+
+# %% Plot: hist of last acc_mean
+
+fig, axs = plt.subplots(1, 2, sharex=True, sharey=True)
+
+ax_no = plot_acc_hist(trials_NO_results_acc_hist_list[3], ax=axs[0])
+ax_no.set_title("No plasticity")
+ax_yes = plot_acc_hist(trials_YES_results_acc_hist_list[3], ax=axs[1])
+ax_yes.set_title("With plasticity")
+fig.savefig(EXP_REPORT_DIR/"compare-examples-plasticity_acc_hist-plot.png")
+
+fig, axs = plt.subplots(1, 2, sharex=True, sharey=True)
+axs[0].hist(trials_NO_results_acc_last_mean, bins=20)
+axs[0].set_title("No plasticity")
+axs[0].axvline(np.array(trials_YES_results_acc_last_mean).mean(), color="grey")
+axs[1].hist(trials_YES_results_acc_last_mean, bins=20)
+axs[1].set_title("With plasticity")
+axs[1].axvline(np.array(trials_YES_results_acc_last_mean).mean(), color="grey")
+fig.savefig(EXP_REPORT_DIR/"compare-examples-plasticity_last_acc_mean-hist.png")
+
+# %% Plot band of the acc
+
+from scipy.ndimage import gaussian_filter1d
+
+fig, axs = plt.subplots(1, 2, sharey=True)
+
+# smooth=True
+trials_YES_results_acc_hist = torch.stack(trials_YES_results_acc_hist_list)
+
+plot_signal_band_plot(trials_YES_results_acc_hist, ax=axs[0])
+axs[0].set_ylabel("Accuracy")
+axs[0].set_xlabel("acc scope, after wash time")
+axs[0].set_title("With plasticity")
+
+trials_NO_results_acc_hist = torch.stack(trials_NO_results_acc_hist_list)
+
+plot_signal_band_plot(trials_NO_results_acc_hist, ax=axs[1])
+axs[1].set_ylabel("Accuracy")
+axs[1].set_xlabel("acc scope, after wash time")
+axs[1].set_title("Without plasticity")
+
+fig.tight_layout()
+
+# %%
+
+SAVE_DATA and fig_pos_perf.savefig(OUTPUT_DIR/f"compare-perf_x_pos-mean.png")
+
+# %%
+
+start_from = 10_000
+fig, axs = plt.subplots(1, 2, figsize=(12, 4), sharey=True)
+
+sig_len = trials_NO_results_acc_hist[:, start_from:].shape[1]
+plot_signal_band_plot(trials_NO_results_acc_hist[:, start_from:], ax=axs[1])
+axs[0].set_ylabel("Accuracy")
+axs[0].set_xlabel("acc scope, after wash time")
+axs[0].set_title("Without plasticity")
+axs[0].set_xlim(0, sig_len)
+
+plot_signal_band_plot(trials_YES_results_acc_hist[:, start_from:], ax=axs[0])
+axs[1].set_ylabel("Accuracy")
+axs[1].set_xlabel("acc scope, after wash time")
+axs[1].set_title("With plasticity")
+
+fig.suptitle(f"Average Accuracy. Last {sig_len} windows")
+
+fig.tight_layout()
+
+# %%
+
+fig, ax = plt.subplots()
+
+plot_signal_band_plot(trials_NO_results_acc_hist[:, start_from:], ax=ax)
+plot_signal_band_plot(trials_YES_results_acc_hist[:, start_from:], ax=ax)
+ax.set_ylabel("Accuracy")
+ax.set_xlabel("acc scope, after wash time")
+ax.set_title("Without plasticity")
+ax.set_xlim(0, sig_len)
+ax.legend(["No plasticty", "With plasticity"])
+
+# %%
+
+exit()
 
 # %% -- Analyze --
 
